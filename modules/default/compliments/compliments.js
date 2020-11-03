2c0354b801fbbfafcb1d06131f23759f20b1a396
aliff <alifzulkifeli@gmail.com>
2020-11-04 07:56:50 +0900

    FEATURE: add language support for japanese language

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,10 +8,10 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hello Aliff!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Hello Aliff!", "Hello Aliff!", "Hello Aliff!"],
+			anytime: ["こんにちは亀田先生"],
+			morning: ["おはようございます"],
+			afternoon: ["こんにちは"],
+			evening: ["こんにちは"],
 			"....-01-01": ["Happy new year!"]
 		},
 		updateInterval: 30000,
